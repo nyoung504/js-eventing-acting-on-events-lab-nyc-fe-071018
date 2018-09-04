e07d34a71d03e4b1d935b5c5ad622f2e8c01dccd
Nicole Young <coley504@gmail.com>
2018-09-04 22:40:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = document.getElementById('dodger')
+
+document.addEventListener('keydown', (event) => {
+  if (event.which === 37 ) {
+    //'180px'
+    let previousLeft = element.style.left
+  
+    //180
+    let previousLeftNumber = parseInt(previousLeft.replace('px', ''), 10)
+    let newLeftNumber = (previousLeftNumber - 1) + "px"
+    element.style.left = newLeftNumber
+  }
+})
